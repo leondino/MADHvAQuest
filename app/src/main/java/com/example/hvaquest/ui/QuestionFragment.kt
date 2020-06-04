289ddbb80cda2f51f03e9b1d646004e828053faa
Léon Imanuel <leondino0309@gmail.com>
2020-06-04 16:31:35 +0200

    Added finish and clue fragments. Added layouts for all fragments. Added test version of RadioButtons

--- app/src/main/java/com/example/hvaquest/ui/QuestionFragment.kt
+++ app/src/main/java/com/example/hvaquest/ui/QuestionFragment.kt
@@ -5,13 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 
 import com.example.hvaquest.R
+import com.example.hvaquest.model.Question
+import kotlinx.android.synthetic.main.fragment_question.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class QuestionFragment : Fragment() {
+
+    lateinit var radioGroup: RadioGroup
+    lateinit var radioButton: RadioButton
+    lateinit var question: Question
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +33,17 @@ class QuestionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        radioGroup = rgAnswers
+
+        btnConfirm.setOnClickListener { checkButton() }
+    }
+
+    private fun checkButton(){
+        var radioId: Int = radioGroup.checkedRadioButtonId
+
+        radioButton = requireView().findViewById(radioId)
+        Toast.makeText(context, radioButton.text, Toast.LENGTH_LONG).show()
     }
 
 }
