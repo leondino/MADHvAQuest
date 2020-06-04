b88b28f661699d4efd729f013fa83070c3be2277
Léon Imanuel <leondino0309@gmail.com>
2020-06-04 14:59:19 +0200

    Added (blank) question fragment to test if navigation works

--- app/src/main/java/com/example/hvaquest/ui/QuestionFragment.kt
+++ app/src/main/java/com/example/hvaquest/ui/QuestionFragment.kt
@@ -5,29 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 
 import com.example.hvaquest.R
-import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class HomeFragment : Fragment() {
+class QuestionFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_question, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btnStart.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_questionFragment)
-        }
     }
 
 }
