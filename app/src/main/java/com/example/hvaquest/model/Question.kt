0ff61a253e2fbff2ab64a3aebf0323210f9dd665
Léon Imanuel <leondino0309@gmail.com>
2020-06-04 14:02:13 +0200

    Imported given code

---
+++ app/src/main/java/com/example/hvaquest/model/Question.kt
@@ -0,0 +1,11 @@
+package com.example.hvaquest.model
+
+import com.example.hvaquest.R
+
+data class Question (
+    var question: String,
+    var choices: Array<String>,
+    var correctAnswer: String,
+    var clue: Int
+)
+
